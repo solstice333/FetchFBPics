6d09c89e8a90918fa50bb22454e006dc3268ad14
Kevin Navero <knavero@gmail.com>
2018-02-24 13:12:33 -0800

    first commit

---
+++ fetch_fb_pics.js
@@ -0,0 +1,3 @@
+var pics = document.querySelectorAll("._4dy9 a");
+for (let p of pics)
+   console.log(p.href)
---
+++ fetch_fb_pics.py
@@ -0,0 +1,103 @@
+from urllib.parse import urlparse
+
+import urllib.request
+import re
+import argparse
+import os.path
+import ssl
+
+
+class URLFileParser:
+   def __init__(self, file):
+      self._file = open(file)
+
+   def close(self):
+      self._file.close()
+
+   def get_urls(self):
+      urls = []
+      for line in self._file:
+         for url in re.finditer(r'http\S+', line):
+            urls.append(url.group(0))
+      return urls
+
+   def __enter__(self):
+      return self
+
+   def __exit__(self, *_):
+      self.close()
+
+   def __del__(self):
+      self.close()
+
+class Downloadable:
+   def __init__(self, url):
+      self._url = url
+      self._url_parts = urlparse(self._url)
+      self._default_filename = os.path.basename(self._url_parts.path)
+
+   def download(self, dst='.', custom_filename=None):
+      dst_path = os.path.realpath(
+         os.path.join(dst, custom_filename or self._default_filename))
+      with \
+         urllib.request.urlopen(self._url, context=ssl.SSLContext()) as src,\
+         open(dst_path, 'wb') as dst:
+         dst.write(src.read())
+
+   @property
+   def default_filename(self):
+      return self._default_filename
+
+   @property
+   def url_parts(self):
+      return self._url_parts
+
+   @property
+   def uid(self):
+      return re.search(r'\w+$', self._url_parts.query)[0]
+
+def main():
+   parser = argparse.ArgumentParser(
+      description="parse through URLs and download the content")
+   parser.add_argument('URL_FILE',
+      help='file containing a list of URLs. If the file contains ' +
+         'other things, this tool extracts those tokens that have ' +
+         '"http" in them')
+   parser.add_argument('-d', '--dst', default='.',
+      help='destination folder to store downloads')
+   parser.add_argument('-t', '--tail',
+      help='UID of where to start downloading photos. The UID is the ' +
+         'identifier at the end of the URL at the query part. For example, in ' +
+         'https://scontent-atl3-1.xx.fbcdn.net/v/t34.0-12/' +
+         '28313230_10215999386641579_1936966559_n.jpg' +
+         '?oh=13b2d5a06c227a937a97229a0a61eaeb&oe=5A937038, the UID here is ' +
+         '5A937038. Downloads all photos by default')
+   parser.add_argument('-p', '--print-ids', action='store_true',
+      help='just print uids')
+   args = parser.parse_args()
+
+   with URLFileParser(args.URL_FILE) as urp:
+      start_capture = False
+
+      for url in urp.get_urls():
+         dl = Downloadable(url)
+
+         if args.print_ids:
+            print(dl.uid)
+
+         elif args.tail is None or \
+            args.tail and dl.uid == args.tail:
+            start_capture = True
+
+         if start_capture:  
+            print("downloading {}...".format(dl.default_filename), end='')
+            while True:
+               try:
+                  dl.download(args.dst)
+                  break
+               except urllib.error.URLError as err:
+                  print(err.reason)
+            print("done!")
+
+if __name__ == '__main__':
+   main()
